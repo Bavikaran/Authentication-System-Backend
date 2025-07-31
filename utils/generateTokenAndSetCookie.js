0444dabecbd4611272fac7dcc697efbedc39d641
Bavikaran <bavikaran01@gmail.com>
2025-07-31 18:02:53 +0530

    token generation cookie setting created

---
+++ utils/generateTokenAndSetCookie.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+export const generateTokenAndSetCookie = (res, userId) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "7d", // Token expires in 7 days
+    });
+
+    res.cookie("token", token, {
+        httpOnly: true, // Prevents JavaScript access to cookie
+        secure: process.env.NODE_ENV === "production", // Use HTTPS in production
+        sameSite: "strict", // Protects against CSRF
+        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days in milliseconds
+    });
+
+    return token;
+};
