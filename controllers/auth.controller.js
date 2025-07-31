43191fd738a94656268eb1645e82732788dc2cf7
Bavikaran <bavikaran01@gmail.com>
2025-07-31 18:24:01 +0530

    email verification endpoint created

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -35,7 +35,7 @@ export const signup = async (req, res) => {
     
 
 
-    
+
 
     res.status(201).json({
       success: true,
@@ -48,5 +48,45 @@ export const signup = async (req, res) => {
 
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+
+
+
+
+
+export const verifyEmail = async (req, res) => {
+  const { code } = req.body;
+
+  try {
+    const user = await User.findOne({
+      verificationToken: code,
+      verificationTokenExpiresAt: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res.status(400).json({ success: false, message: "Invalid or expired verification code" });
+    }
+
+    user.isVerified = true;
+    user.verificationToken = undefined;
+    user.verificationTokenExpiresAt = undefined;
+    await user.save();
+
+   
+
+    res.status(200).json({
+      success: true,
+      message: "Email verified successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+
+  } catch (error) {
+    console.log("error in verifyEmail", error);
+    res.status(500).json({ success: false, message: "Server error" });
   }
 };
