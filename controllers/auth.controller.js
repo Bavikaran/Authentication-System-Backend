c5ff3a482751d92e5c621e9966639fb7df1fe14f
Bavikaran <bavikaran01@gmail.com>
2025-07-31 17:38:40 +0530

    signup endpoint created

---
+++ controllers/auth.controller.js
@@ -0,0 +1,52 @@
+import { User } from '../models/user.model.js';
+import crypto from "crypto";
+import bcryptjs from 'bcryptjs';
+
+
+
+
+export const signup = async (req, res) => {
+  const { email, password, name, userType } = req.body;
+
+  try {
+    if (!email || !password || !name || !userType) {
+      throw new Error("All fields are required");
+    }
+
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      return res.status(400).json({ success: false, message: "User already exists" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      userType,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000 // 24 hours
+    });
+
+    await user.save();
+
+    
+
+
+    
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
