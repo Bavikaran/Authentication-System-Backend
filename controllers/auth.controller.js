7c3679308fddd5e81c284f0a1c12904bbacf8a7c
Diron714 <louisdiron2002@gmail.com>
2025-08-01 18:22:28 +0530

    Add logout functionality to auth controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -123,6 +123,13 @@ export const login = async (req, res) => {
 };
 
 
+export const logout = async (req, res) => {
+  res.clearCookie("token");
+  res.status(200).json({ success: true, message: "Logged out successfully" });
+};
+
+
+
 
 
 
