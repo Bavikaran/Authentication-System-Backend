a55a3b570eb3155ebbf686fab1ccc3c26ba74ada
Thiruchelvam Janagan <144086848+ThiruJanakan@users.noreply.github.com>
2025-08-02 11:24:59 +0530

    Refactor index.js for improved readability and organization

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-import express from 'express';  //express is the main web framework used to create HTTP servers in Node.js.
-import { connectDB } from './db/connectDB.js'; //connectDB is a custom function (you likely defined in db/connectDB.js) used to connect to MongoDB
-import dotenv from 'dotenv'; // import enviorment variable
+import express from 'express';  
+import { connectDB } from './db/connectDB.js'; 
+import dotenv from 'dotenv'; 
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auth.route.js';
 
-dotenv.config(); // Load environment variables from .env file
-const app = express(); // Create an instance of an Express application
+dotenv.config();
+const app = express(); 
 
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true
 }));
 
-const PORT = process.env.PORT || 5000; //define server port
+const PORT = process.env.PORT || 5000; 
 
-app.use(express.json()); // Middleware to parse JSON request bodies
+app.use(express.json()); 
 app.use(cookieParser());
+app.use('/api/auth', authRoutes); 
 
 
 
-app.listen(PORT, () => {// expree server port
-  connectDB(); // connect db
-  console.log("Server running on port:", PORT);// console message
+app.listen(PORT, () => {
+  connectDB(); 
+  console.log("Server running on port:", PORT);
 });
